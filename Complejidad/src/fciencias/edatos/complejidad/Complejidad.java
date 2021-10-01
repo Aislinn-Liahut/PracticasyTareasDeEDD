13fb50e230f1c55987f36b7e4bbbbc6a47c2d1f2
Aislinn-Liahut <liahutaislinn@ciencias.unam.mx>
2021-10-01 03:58:33 -0500

    3ermetdo

--- Complejidad/src/fciencias/edatos/complejidad/Complejidad.java
+++ Complejidad/src/fciencias/edatos/complejidad/Complejidad.java
@@ -47,6 +47,56 @@ public class Complejidad {
   }
 
   /**
+	* Hace la mezcla de dos arreglos ordenados desde la primera posición hasta
+	* una posición límite (De manera más optima)
+	* @param array1 el primer arreglo a mezlar
+	* @param n el límite de mezcla del primer arreglo
+	* @param array2 el segundo arreglo a mezclar
+	* @param m el límite de mezcla del segundo arreglo.
+	* @return un arreglo ordenado de longitud m+n con la mezcla definida.
+	*/
+	public static int[] mergeSortedArrayOptimizado(int[] array1, int n, int[] array2, int m){
+    if (n > array1.length || m > array2.length) throw new RuntimeException(
+      "Límites no válidos"
+    );
+
+    int[] result = new int[n + m];
+    int pn=0; // Apuntador del arreglo 1
+    int pm=0; // Apuntador del arreglo 2
+    int pr=0; // Apuntador del arreglo result
+		
+    // Acomodar y comparar al mismo tiempo
+    for(;pr<=result.length-1;pr++){
+    
+       
+      // Por si pn ya reccorio hasta pn-1 y pm aún no
+      if(pn==n){
+        result[pr]=array2[pm];
+        pm++;
+      continue; // Para continuar con las iteraciones solo agregando los elementos de array 2 
+      }
+      // Por si pm ya reccorio hasta pm-1 y pn aún no
+      if(pm==m){
+        result[pr]=array1[pn];
+        ++pn;
+      continue; // Para continuar con las iteraciones solo agregando los elementos de array 1 
+      }
+      
+      //
+      if(array1[pn]<array2[pm]){
+        result[pr]=array1[pn];
+        ++pn;
+      }
+
+      else{
+        result[pr]=array2[pm];
+        pm++;
+      }
+    }
+      
+		return result;
+	}
+  /**
    * Verifica si un tablero contiene los números desde 0 hasta n-1 en cada fila y cada columna.
    * @param board el tablero de nxn que contiene elementos dentro del rango [0, n-1].
    * @return true si el tablero contiene los números desde 0 hasta n-1 en cada fila y columna,
@@ -131,23 +181,60 @@ public class Complejidad {
     }
   }
 
+  /**
+   * Rota position cantidad de veces los elementos de un arreglo
+   * hacia el vecino izquierdo.
+   * @param num el arreglo de enteros a rotar.
+   * @param position la cantidad de espacios a rotar.
+   */
+  public static void rotateArrayOptimizado(int[] num, int position) {
+    int[] nuevo = new int[num.length];
+   
+
+    int modulo = position % num.length;
+
+    for(int i = 0; i< num.length; i++ ){
+    //  System.out.println("es" +modulo+"");
+      if(modulo == num.length){
+        modulo = 0;
+      
+      }
+
+  
+      nuevo[i] = num[modulo];
+      //System.out.print(""+nuevo[i]+"");
+     
+   
+      modulo++;
+     
+      
+    }
+    num=nuevo;
+
+    
+  }
+
+
+  //13,29,58,59,74,90,91
+  
+
   public static void main(String[] args) {
-    /*  String directorio1 = "Examples/ArrayExamples/";
-    String directorio2 = "Examples/BoardExamples/"; */
+  /*   String directorio1 = "Examples/ArrayExamples/";
+    String directorio2 = "Examples/BoardExamples/";  */
 
     String directorio1 = "Tests/ArrayTests/";
-    String directorio2 = "Tests/BoardTests/";
+    String directorio2 = "Tests/BoardTests/"; 
 
     // EJEMPLOS DE ACTIVIDAD 1
     System.out.println("\nEJEMPLOS DE ACTIVIDAD 1\n");
-
+ 
     ///--- Primer algoritmo
     int[] arrayA1 = ArrayReader.readArray(directorio1 + "ArrayA1.txt");
     int[] arrayA2 = ArrayReader.readArray(directorio1 + "ArrayA2.txt");
     long inicio = System.currentTimeMillis();
-    int[] resultA = mergeSortedArray(arrayA1, 3, arrayA2, 5);
+    int[] resultA = mergeSortedArray(arrayA1, 500, arrayA2, 700);
     long fin = System.currentTimeMillis();
-    System.out.println("Resultado A: " + Arrays.toString(resultA));
+    //System.out.println("Resultado A: " + Arrays.toString(resultA));
     System.out.println(
       "El algoritmo 1 se tardó: " + (fin - inicio) + " milisegundos."
     );
@@ -155,9 +242,9 @@ public class Complejidad {
     int[] arrayB1 = ArrayReader.readArray(directorio1 + "ArrayB1.txt");
     int[] arrayB2 = ArrayReader.readArray(directorio1 + "ArrayB2.txt");
     inicio = System.currentTimeMillis();
-    int[] resultB = mergeSortedArray(arrayB1, 5, arrayB2, 5);
+    int[] resultB = mergeSortedArray(arrayB1, 2000, arrayB2, 3500);
     fin = System.currentTimeMillis();
-    System.out.println("Resultado B: " + Arrays.toString(resultB));
+    //System.out.println("Resultado B: " + Arrays.toString(resultB));
     System.out.println(
       "El algoritmo 1 se tardó: " + (fin - inicio) + " milisegundos."
     );
@@ -165,9 +252,9 @@ public class Complejidad {
     int[] arrayC1 = ArrayReader.readArray(directorio1 + "ArrayC1.txt");
     int[] arrayC2 = ArrayReader.readArray(directorio1 + "ArrayC2.txt");
     inicio = System.currentTimeMillis();
-    int[] resultC = mergeSortedArray(arrayC1, 4, arrayC2, 6);
+    int[] resultC = mergeSortedArray(arrayC1, 4000, arrayC2, 4000);
     fin = System.currentTimeMillis();
-    System.out.println("Resultado C: " + Arrays.toString(resultC));
+    //System.out.println("Resultado C: " + Arrays.toString(resultC));
     System.out.println(
       "El algoritmo 1 se tardó: " + (fin - inicio) + " milisegundos."
     );
@@ -175,9 +262,9 @@ public class Complejidad {
     int[] arrayD1 = ArrayReader.readArray(directorio1 + "ArrayD1.txt");
     int[] arrayD2 = ArrayReader.readArray(directorio1 + "ArrayD2.txt");
     inicio = System.currentTimeMillis();
-    int[] resultD = mergeSortedArray(arrayA1, 3, arrayD2, 5);
+    int[] resultD = mergeSortedArray(arrayD1, 7000, arrayD2, 8000);
     fin = System.currentTimeMillis();
-    System.out.println("Resultado D: " + Arrays.toString(resultD));
+    //System.out.println("Resultado D: " + Arrays.toString(resultD));
     System.out.println(
       "El algoritmo 1 se tardó: " + (fin - inicio) + " milisegundos."
     );
@@ -185,9 +272,9 @@ public class Complejidad {
     int[] arrayE1 = ArrayReader.readArray(directorio1 + "ArrayE1.txt");
     int[] arrayE2 = ArrayReader.readArray(directorio1 + "ArrayE2.txt");
     inicio = System.currentTimeMillis();
-    int[] resultE = mergeSortedArray(arrayB1, 5, arrayE2, 5);
+    int[] resultE = mergeSortedArray(arrayE1, 15000, arrayE2, 19000);
     fin = System.currentTimeMillis();
-    System.out.println("Resultado E: " + Arrays.toString(resultE));
+    //System.out.println("Resultado E: " + Arrays.toString(resultE));
     System.out.println(
       "El algoritmo 1 se tardó: " + (fin - inicio) + " milisegundos."
     );
@@ -195,9 +282,9 @@ public class Complejidad {
     int[] arrayF1 = ArrayReader.readArray(directorio1 + "ArrayF1.txt");
     int[] arrayF2 = ArrayReader.readArray(directorio1 + "ArrayF2.txt");
     inicio = System.currentTimeMillis();
-    int[] resultF = mergeSortedArray(arrayF1, 4, arrayF2, 6);
+    int[] resultF = mergeSortedArray(arrayF1, 30000, arrayF2, 25000);
     fin = System.currentTimeMillis();
-    System.out.println("Resultado F: " + Arrays.toString(resultF));
+    //System.out.println("Resultado F: " + Arrays.toString(resultF));
     System.out.println(
      "El algoritmo 1 se tardó: " + (fin - inicio) + " milisegundos."
     );
@@ -205,56 +292,56 @@ public class Complejidad {
     //--- Segundo algoritmo
 
     inicio = System.currentTimeMillis();
-    int[] resultA2 = mergeSortedArray(arrayA1, 3, arrayA2, 5);
+    int[] resultA2 = mergeSortedArrayOptimizado(arrayA1, 500, arrayA2, 700);
     fin = System.currentTimeMillis();
-    System.out.println("Resultado A: " + Arrays.toString(resultA2));
+    //System.out.println("Resultado A: " + Arrays.toString(resultA2));
     System.out.println(
       "El algoritmo 2 se tardó: " + (fin - inicio) + " milisegundos."
     );
 
     inicio = System.currentTimeMillis();
-    int[] resultB2 = mergeSortedArray(arrayB1, 5, arrayB2, 5);
+    int[] resultB2 = mergeSortedArrayOptimizado(arrayB1, 2000, arrayB2, 3500);
     fin = System.currentTimeMillis();
-    System.out.println("Resultado B: " + Arrays.toString(resultB2));
+    //System.out.println("Resultado B: " + Arrays.toString(resultB2));
     System.out.println(
       "El algoritmo 2 se tardó: " + (fin - inicio) + " milisegundos."
     );
 
     inicio = System.currentTimeMillis();
-    int[] resultC2 = mergeSortedArray(arrayC1, 4, arrayC2, 6);
+    int[] resultC2 = mergeSortedArrayOptimizado(arrayC1, 4000, arrayC2, 4000);
     fin = System.currentTimeMillis();
-    System.out.println("Resultado C: " + Arrays.toString(resultC2));
+    //System.out.println("Resultado C: " + Arrays.toString(resultC2));
     System.out.println(
       "El algoritmo 2 se tardó: " + (fin - inicio) + " milisegundos."
     );
 
     inicio = System.currentTimeMillis();
-    int[] resultD2 = mergeSortedArray(arrayA1, 3, arrayD2, 5);
+    int[] resultD2 = mergeSortedArrayOptimizado(arrayD1, 7000, arrayD2, 8000);
     fin = System.currentTimeMillis();
-    System.out.println("Resultado D: " + Arrays.toString(resultD2));
+    //System.out.println("Resultado D: " + Arrays.toString(resultD2));
     System.out.println(
       "El algoritmo 2 se tardó: " + (fin - inicio) + " milisegundos."
     );
 
     inicio = System.currentTimeMillis();
-    int[] resultE2 = mergeSortedArray(arrayB1, 5, arrayE2, 5);
+    int[] resultE2 = mergeSortedArrayOptimizado(arrayE1, 15000, arrayE2, 19000);
     fin = System.currentTimeMillis();
-    System.out.println("Resultado E: " + Arrays.toString(resultE2));
+    //System.out.println("Resultado E: " + Arrays.toString(resultE2));
     System.out.println(
       "El algoritmo 2 se tardó: " + (fin - inicio) + " milisegundos."
     );
 
     inicio = System.currentTimeMillis();
-    int[] resultF2 = mergeSortedArray(arrayF1, 4, arrayF2, 6);
+    int[] resultF2 = mergeSortedArrayOptimizado(arrayF1, 30000, arrayF2, 25000);
     fin = System.currentTimeMillis();
-    System.out.println("Resultado F: " + Arrays.toString(resultF2));
+    //System.out.println("Resultado F: " + Arrays.toString(resultF2));
     System.out.println(
       "El algoritmo 2 se tardó: " + (fin - inicio) + " milisegundos."
     );
 
     // EJEMPLOS DE ACTIVIDAD 2
     System.out.println("\nEJEMPLOS DE ACTIVIDAD 2\n");
-
+/* 
     int[][] boardA = ArrayReader.readMatrix(directorio2 + "BoardA.txt");
     inicio = System.currentTimeMillis();
     boolean boardResultA = isValidBoard(boardA);
@@ -364,7 +451,7 @@ public class Complejidad {
     System.out.println(
       "El algoritmo 2 se tardó: " + (fin - inicio) + " milisegundos."
     );
-
+  
     // EJEMPLOS DE ACTIVIDAD 3
     System.out.println("\nEJEMPLOS DE ACTIVIDAD 3\n");
 
@@ -435,7 +522,7 @@ public class Complejidad {
     //Segundo algoritmo----------------------------------
 
     inicio = System.currentTimeMillis();
-    rotateArray(arrayA1, 5);
+    rotateArrayOptimizado(arrayA1, 5);
     fin = System.currentTimeMillis();
     System.out.println(
       "Arreglo A1 rotado 5 veces: " + Arrays.toString(arrayA1)
@@ -446,7 +533,7 @@ public class Complejidad {
 
 
     inicio = System.currentTimeMillis();
-    rotateArray(arrayB1, 0);
+    rotateArrayOptimizado(arrayB1, 0);
     fin = System.currentTimeMillis();
     System.out.println(
       "Arreglo B1 rotado 0 veces: " + Arrays.toString(arrayB1)
@@ -456,7 +543,7 @@ public class Complejidad {
     );
 
     inicio = System.currentTimeMillis();
-    rotateArray(arrayC1, 6);
+    rotateArrayOptimizado(arrayC1, 6);
     fin = System.currentTimeMillis();
     System.out.println(
       "Arreglo C1 rotado 6 veces: " + Arrays.toString(arrayC1)
@@ -466,7 +553,7 @@ public class Complejidad {
     );
 
     inicio = System.currentTimeMillis();
-    rotateArray(arrayD1, 5);
+    rotateArrayOptimizado(arrayD1, 5);
     fin = System.currentTimeMillis();
     System.out.println(
       "Arreglo D1 rotado 5 veces: " + Arrays.toString(arrayD1)
@@ -476,7 +563,7 @@ public class Complejidad {
     );
 
     inicio = System.currentTimeMillis();
-    rotateArray(arrayE1, 0);
+    rotateArrayOptimizado(arrayE1, 0);
     fin = System.currentTimeMillis();
     System.out.println(
       "Arreglo E1 rotado 0 veces: " + Arrays.toString(arrayE1)
@@ -487,7 +574,7 @@ public class Complejidad {
     );
 
     inicio = System.currentTimeMillis();
-    rotateArray(arrayF1, 6);
+    rotateArrayOptimizado(arrayF1, 6);
     fin = System.currentTimeMillis();
     System.out.println(
       "Arreglo F1 rotado 6 veces: " + Arrays.toString(arrayF1)
@@ -496,6 +583,25 @@ public class Complejidad {
       "El algoritmo 2 se tardó: " + (fin - inicio) + " milisegundos."
     );
 
-    System.out.println("\n\nFIN DE EJEMPLOS :) \n");
+    System.out.println("\n\nFIN DE EJEMPLOS :) \n");  */
+
+    // EJEMPLOS DE ACTIVIDAD 3
+	/* 	System.out.println("\nEJEMPLOS DE ACTIVIDAD 3\n");
+    int[] arrayA1 = ArrayReader.readArray(directorio1 + "ArrayA1.txt");
+    int[] arrayB1 = ArrayReader.readArray(directorio1 + "ArrayB1.txt");
+
+    rotateArrayOptimizado(arrayA1, 5);
+	
+
+		rotateArray(arrayB1, 0);
+
+		System.out.println("Arreglo A1 rotado 5 veces: " + Arrays.toString(arrayA1));
+    rotateArrayOptimizado(arrayA1, 8);
+    System.out.println("Arreglo A1 rotado 8 veces: " + Arrays.toString(arrayA1));
+    rotateArrayOptimizado(arrayA1, 12);
+    System.out.println("Arreglo A1 rotado 12 veces: " + Arrays.toString(arrayA1));
+		System.out.println("Arreglo B1 rotado 0 veces: " + Arrays.toString(arrayB1));
+	 */
+
   }
 }
